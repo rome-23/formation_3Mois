22aa013b816b3683638a5a751f52a5cde89e06e7
Romeo Loukou <loukour4@gmail.com>
2026-09-17 13:27:29 +0300

    First_file.py

--- First_file.py
+++ First_file.py
@@ -1,4 +1,30 @@
+
+"""
 age = 23
 nom = "romeo"
 print(age, nom)
 print(" i an strong in agent AI")
+"""
+
+
+"""
+n = -7 
+if n <= 0:
+    print("Negative")
+elif n >= 0:
+    print("Positive")
+else:
+    print("Zero")
+
+"""
+
+
+"""
+total = 0
+for i in range(1, 101):
+    total = total + i
+print(total)
+
+"""
+
+
